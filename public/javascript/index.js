8644c85be2d371d43a3bc2ad268013d8e7ca2ceb
Isak La Fleur <isak.engdahl@gmail.com>
2017-11-14 19:50:51 +0100

    moved html to index.ejs plus some other small fixes.

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,9 +1,10 @@
-const charactersAPI = new APIHandler('http://ih-api.herokuapp.com/characters');
-
+const charactersAPI = new APIHandler(
+  "http://ih-crud-api.herokuapp.com/characters"
+);
 
 $(document).ready(() => {
   function addCharacter(char) {
-    $('.characters-container').append(`
+    $(".characters-container").append(`
     <div class="character-info">
       <div class="name">ID: ${char.id}</div>
       <div class="name">Name: ${char.name}</div>
@@ -15,70 +16,74 @@ $(document).ready(() => {
   }
 
   function clearHistory() {
-    $('.characters-container').html('');
+    $(".characters-container").html("");
   }
 
-  $('#fetch-all').on('click', (e) => {
-    charactersAPI.getFullList((response) => {
+  $("#fetch-all").on("click", e => {
+    charactersAPI.getFullList(response => {
       clearHistory();
       // console.log("response", response);
       response.forEach(character => addCharacter(character));
     });
   });
 
-  $('#fetch-one').on('click', (e) => {
+  $("#fetch-one").on("click", e => {
     const charId = $("input[name='character-id']").val();
-    charactersAPI.getOneRegister(charId, (response) => {
+    charactersAPI.getOneRegister(charId, response => {
       clearHistory();
       addCharacter(response);
     });
   });
 
-  $('#delete-one').on('click', (e) => {
+  $("#delete-one").on("click", e => {
     const charId = $("input[name='character-id-delete']").val();
-    charactersAPI.deleteOneRegister(charId, (response) => {
+    charactersAPI.deleteOneRegister(charId, response => {
       // console.log(response.error);
       if (response.error) {
-        $('#delete-one').css('background.color', 'red');
+        $("#delete-one").css("background.color", "red");
       } else {
-        $('#delete-one').css('background.color', 'green');
+        $("#delete-one").css("background.color", "green");
       }
     });
   });
 
-  $('#edit-character-form').on('submit', (e) => {
+  $("#edit-character-form").on("submit", e => {
     e.preventDefault();
-    const charId = $('#edit-character-form input[name=chr-id]').val();
+    const charId = $("#edit-character-form input[name=chr-id]").val();
     const char = {
-      name: $('#edit-character-form input[name=name]').val(),
-      occupation: $('#edit-character-form input[name=occupation]').val(),
-      weapon: $('#edit-character-form input[name=weapon]').val(),
-      debt: $('#edit-character-form input[type=checkbox][name=debt]:checked').val(),
+      name: $("#edit-character-form input[name=name]").val(),
+      occupation: $("#edit-character-form input[name=occupation]").val(),
+      weapon: $("#edit-character-form input[name=weapon]").val(),
+      debt: $(
+        "#edit-character-form input[type=checkbox][name=debt]:checked"
+      ).val()
     };
-    charactersAPI.updateOneRegister(charId, char, (response) => {
+    charactersAPI.updateOneRegister(charId, char, response => {
       if (response.error) {
-        $('#edit-character-form button').css('background-color', 'red');
+        $("#edit-character-form button").css("background-color", "red");
       } else {
-        $('#edit-character-form button').css('background-color', 'green');
+        $("#edit-character-form button").css("background-color", "green");
         clearHistory();
         addCharacter(response);
       }
     });
   });
 
-  $('#new-character-form').on('submit', (e) => {
+  $("#new-character-form").on("submit", e => {
     e.preventDefault();
     const characterInfo = {
-      name: $('#new-character-form input[name=name]').val(),
-      occupation: $('#new-character-form input[name=occupation]').val(),
-      weapon: $('#new-character-form input[name=weapon]').val(),
-      debt: $('#new-character-form input[type=checkbox][name=debt]:checked').val(),
+      name: $("#new-character-form input[name=name]").val(),
+      occupation: $("#new-character-form input[name=occupation]").val(),
+      weapon: $("#new-character-form input[name=weapon]").val(),
+      debt: $(
+        "#new-character-form input[type=checkbox][name=debt]:checked"
+      ).val()
     };
-    charactersAPI.createOneRegister(characterInfo, (response) => {
+    charactersAPI.createOneRegister(characterInfo, response => {
       if (response.error) {
-        $('#new-character-form button').css('background-color', 'red');
+        $("#new-character-form button").css("background-color", "red");
       } else {
-        $('#new-character-form button').css('background-color', 'green');
+        $("#new-character-form button").css("background-color", "green");
         clearHistory();
         addCharacter(response);
       }
